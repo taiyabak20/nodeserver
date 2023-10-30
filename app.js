3ded73726a1e3c1c4ec89cc6704f788e4e120ddf
taiyabak20 <taiyabak20@gmail.com>
2023-10-30 14:42:08 +0530

    returning response

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // Get the URL from the request
-    
     console.log(req.url, req.method, req.headers);
 
     // Set the response headers
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
 
     // Check the URL and send custom responses
     
     if (req.url === '/home') {
-        res.setHeader('content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>my home page</title></head>');
         res.write('<body>Welcome home</body>');
         res.write('</html>');
         res.end();
     } else if (req.url === '/about') {
-        res.setHeader('content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>About Us</title></head>');
         res.write('<body>Welcome to About Us page</body>');
         res.write('</html>');
         res.end();
     } else if (req.url === '/node') {
-        res.setHeader('content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>node page</title></head>');
         res.write('<body>Welcome to my Node Js project</body>');
         res.write('</html>');
         res.end();
     } else {
-        res.setHeader('content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>node js project</title></head>');
         res.write('<body>Welcome to my Node Js project</body>');
