7187e7c2207d61ef01924cfb34a51c9dda1d427e
taiyabak20 <taiyabak20@gmail.com>
2023-11-07 17:27:19 +0530

    body-parser

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require ('express')
-
+const bodyparser = require('body-parser')
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware')
-    next();
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name = "title" placeholder="title"><input type="text" name = "size" placeholder="size"><input type = "submit"></form>')
+    
 })
 
-app.use((req, res, next)=>{
-    console.log('In another middleware')
+app.post('/product',(req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/');
+
+})
+app.use('/',(req, res, next)=>{
     res.send('<h1>Hello taiyaba!</h1>')
 })
 
-app.listen(3000);
+app.listen(4000);
